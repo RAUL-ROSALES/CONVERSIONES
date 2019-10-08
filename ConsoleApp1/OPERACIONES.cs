367eaab98a0617faf6a8fca8440f19c2d8e46094
RAUL-ROSALES <55122080+RAUL-ROSALES@users.noreply.github.com>
2019-10-08 10:32:41 -0700

    Add files via upload

---
+++ ConsoleApp1/OPERACIONES.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /*creo una clase para realizar las conversiones */
+   public class OPERACIONES
+    {
+        public double Conversion(double x)//aqui empiezo a hacer sobre carga de metodos 
+        {
+            /*aqui empiezo a hacer la operacion
+             de centimetros a metros */
+            double res = x / 100;
+            return res;
+        }
+        public double Conversion(double x,double xx)//este es el segundo metodo sobrecargado
+        {
+            /*y aqui hago la conversion de 
+             metros a pulgadas multiplicando los metros que ingreso el usuario por
+             lo */
+            double res = xx * 39.3701;
+            return res;
+        }
+        public double Conversion(double x,double xx,double xxx)
+        {
+            double res = xxx * 1000;
+            return res;
+        }
+    }
+}
---
+++ ConsoleApp1/Prinipal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+   public class Prinipal
+   {
+        public void Bienvenida()
+        {
+            /*creo el metodo bienvenida para que este sea el unico metodo que instancie en
+             el main*/
+            Console.WriteLine("-----Bienvenido a mi Humilde Programita-----");
+            Menu(); //mando a llamar al metodo menu para que se ejecute    
+        }
+        public void Menu()
+        {
+            /*aqui le doy la opcines que existen y que elija la opcion que elija */
+            Console.WriteLine();
+            Console.WriteLine("=====Opcines de conversiones=====");
+            Console.WriteLine();
+            Console.WriteLine("1-Convercion de CENTIMETROS a METROS ");
+            Console.WriteLine();
+            Console.WriteLine("2-Conversion de METROS a PULGADAS");
+            Console.WriteLine();
+            Console.WriteLine("3-Conversion de Litros a Mililitos");
+            Console.WriteLine();
+            Opciones();//mando a llamar al metodo opciones
+            Console.WriteLine();
+        }
+        public void Opciones()
+        {
+            Console.WriteLine("Eija la operacion que desea realizar");
+            int opc = Convert.ToInt32(Console.ReadLine());
+
+            OPERACIONES o = new OPERACIONES();
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine("Ingrese Centimetros");
+                    double Cm = Convert.ToDouble(Console.ReadLine());
+                    double res = o.Conversion(Cm);
+                    Console.WriteLine("la Conversion de {0} cm a metro(s) es {1} metros", Cm, res);
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese los Metros");
+                    double m = Convert.ToDouble(Console.ReadLine());
+                    double res2 = o.Conversion(0, m);
+                    Console.WriteLine("la Conversion de {0} metro(s) a pulgadas es {1} pulgadas", m, res2);
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese los litros");
+                    double litros = Convert.ToDouble(Console.ReadLine());
+                    double res3 = o.Conversion(0, 0, litros);
+                    Console.WriteLine("La conversion de {0} litro(s) a mililitros es {1} mililitros", litros, res3);
+                    break;
+                default:
+                    Console.WriteLine("opcion incorrecta");
+                    Menu();
+                    break;
+            }
+        }
+   }
+}
